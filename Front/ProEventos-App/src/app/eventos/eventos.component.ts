ee5001f7aebb26bf89d0d14274c6b8e00c7defe7
Anderson <ped2.nexsolar@gmail.com>
2021-12-20 23:11:14 -0300

    Seção 5 - Angular: Interpolação, Diretiva e Biding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -8,18 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any
+  public eventos : any =[];
+  public eventosFiltrados : any =[];
+  tamanhoImg : number = 150;
+  margemImg : number = 2;
+  exibirImagem = true;
+  private _filtroLista: string = ""
+
+  public get filtroLista() : string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value : string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor : string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase()
+    return this.eventosFiltrados.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getEventos();
+    this.getEventos()
   }
-
-  public getEventos(): void{
+  public getEventos() : void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {this.eventos = response;
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
+
+  public alternarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
+
 }
